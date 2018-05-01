8a0f91749e5adeaf4ea0196d0345636f57594f09
Ken Simiyu <kensimiyu2015@gmail.com>
2018-05-01 18:53:45 +0300

    configure glilde to listen to progress bar before loading images

--- app/src/main/java/kennedysimiyu/com/KabintiStore/databinding/GlildeBindingAdapters.java
+++ app/src/main/java/kennedysimiyu/com/KabintiStore/databinding/GlildeBindingAdapters.java
@@ -5,6 +5,7 @@ import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
 
 import kennedysimiyu.com.KabintiStore.R;
@@ -27,6 +28,25 @@ public class GlildeBindingAdapters {
         Glide.with(context)
                 .setDefaultRequestOptions(options)
                 .load(imageUrl)
+                .into(view);
+
+
+    }
+
+    // using glilde to wait for images to load
+    @BindingAdapter({"requestListener", "imageResource"})
+    public static void bindRequestListener(ImageView view, RequestListener requestListener, int imageResource){
+
+        Context context = view.getContext();
+
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background);
+
+        Glide.with(context)
+                .setDefaultRequestOptions(options)
+                .load(imageResource)
+                .listener(requestListener)
                 .into(view);
 
 
