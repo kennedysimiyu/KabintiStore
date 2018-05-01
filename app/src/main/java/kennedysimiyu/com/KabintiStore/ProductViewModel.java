5464cd9b36940dae5236b4645da9d7bdb920b9e6
Ken Simiyu <kensimiyu2015@gmail.com>
2018-05-01 18:25:42 +0300

    adding progress bar while loading images

--- app/src/main/java/kennedysimiyu/com/KabintiStore/ProductViewModel.java
+++ app/src/main/java/kennedysimiyu/com/KabintiStore/ProductViewModel.java
@@ -2,6 +2,12 @@ package kennedysimiyu.com.KabintiStore;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import kennedysimiyu.com.KabintiStore.models.Product;
 
@@ -13,6 +19,7 @@ public class ProductViewModel extends BaseObservable {
 
     private Product product;
     private  int quantity;
+    private boolean imageVisibility = false;
 
     @Bindable
     public Product getProduct() {
@@ -24,6 +31,11 @@ public class ProductViewModel extends BaseObservable {
         return quantity;
     }
 
+    @Bindable
+    public boolean isImageVisibility() {
+        return imageVisibility;
+    }
+
     public void setProduct(Product product) {
         this.product = product;
         notifyPropertyChanged(BR.product);
@@ -32,5 +44,25 @@ public class ProductViewModel extends BaseObservable {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
         notifyPropertyChanged(BR.quantity);
+    }
+
+    public void setImageVisibility(boolean imageVisibility) {
+        this.imageVisibility = imageVisibility;
+        notifyPropertyChanged(BR.imageVisibility);
+    }
+
+    public RequestListener getCustomRequestListener(){
+        return  new RequestListener() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Object resource, Object model, Target target, DataSource dataSource, boolean isFirstResource) {
+                setImageVisibility(true);
+                return false;
+            }
+        };
     }
 }
